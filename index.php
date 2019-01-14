51fc7171fdc0b59ba68e04a9664ef80b1fef0184
William Isengard <william.isengard@gmail.com>
2019-01-14 16:25:48 -0700

    Changed text and added CSS Ul qualities

--- index.php
+++ index.php
@@ -11,16 +11,16 @@
 		<h1>About Me</h1>
 		<div class="container">
 			<p>Will Tredway is a former carpenter.</p>
-			<p>Standing over <strong>1'4" tall</strong>, he presents the countenance of a <strong><em>mighty tribble!</em></strong></p>
+			<p>Standing over <strong>12" tall</strong>, he presents the embodiment of a <strong><em>mighty tribble!</em></strong></p>
 			<div class="list">
 						<h4>Things Will Likes:</h4>
 						<ul>
-							<li id="item1">pets such as squirrels and falcons</li>
-							<li id="item2">coding</li>
-							<li id="item3">skipping</li>
-							<li id="item4">meditating</li>
+							<li id="item1">rolling around</li>
+							<li id="item2">multiplying</li>
+							<li id="item3">taking up space</li>
+							<li id="item4">harassing klingons</li>
 						</ul>
-				<img src="./resources/tribble.jpg" alt="Will Tredway">
+				<img src="./resources/tribble.jpg" alt="Will Tredway" style="width:500px;height:500px;">
 			</div>
 		</div>
 	</body>
