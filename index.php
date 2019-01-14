63cd7052839e131bf7bad052cc73320a35bb52a3
William Isengard <william.isengard@gmail.com>
2019-01-14 16:44:51 -0700

    Updated text in body

--- index.php
+++ index.php
@@ -12,13 +12,14 @@
 		<div class="container">
 			<p>Will Tredway is a former carpenter.</p>
 			<p>Standing over <strong><em>12" tall</em></strong>, he presents the embodiment of a <strong><em>mighty tribble!</em></strong></p>
+			<p>That's right, Will is a 12" Tribble and former carpenter. Don't hate.</p>
 			<div class="list">
 				<h2>Things Will Likes:</h2>
 					<ul>
-						<li id="item1">rolling around</li>
-						<li id="item2">multiplying</li>
-						<li id="item3">taking up space</li>
-						<li id="item4">harassing klingons</li>
+						<li id="item1">Rolling around doing Tribble stuff.</li>
+						<li id="item2">Multiplying like a Tribble.</li>
+						<li id="item3">Taking up space with my Tribble homies.</li>
+						<li id="item4">Harassing Klingons, as is tradition.</li>
 					</ul>
 			</div>
 			<img src="./resources/tribble.jpg" alt="Will Tredway" style="width:500px;height:500px;">
