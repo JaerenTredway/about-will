ce77e7b3ad1e6f3ee85ea79faf70b9ed4ee7e413
Jaeren William Tredway <jwilliamtredway@gmail.com>
2019-01-14 14:38:48 -0700

    First commit with html and css files.

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+
+	<head>
+		<meta charset="utf-8">
+		<title>About Will</title>
+		<link type="text/css" rel="stylesheet" href="./style.css">
+	</head>
+
+	<body>
+		<h1>About Me</h1>
+		<div class="container">
+			<p>Will Tredway is a former carpenter.</p>
+			<p>Standing over 6'4" tall, he presents the countenance of a mighty giraffe!</p>
+			<div class="#">
+				<ul> Things Will Likes:
+					<li id="item1">pets such as cats and dogs</li>
+					<li id="item2">coding</li>
+					<li id="item3">hiking</li>
+					<li id="item4">meditating</li>
+				</ul>
+				<img src="/Users/jaerentredway/Documents/avatars/avatar_sketch4.png" alt="Will Tredway">
+			</div>
+		</div>
+	</body>
+</html>
