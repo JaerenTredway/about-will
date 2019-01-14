aa33150d879cd46318704612326961e720c1ab96
William Isengard <william.isengard@gmail.com>
2019-01-14 16:35:06 -0700

    Changed h4 to h2 and updated CSS coloring schemes

--- index.php
+++ index.php
@@ -13,15 +13,15 @@
 			<p>Will Tredway is a former carpenter.</p>
 			<p>Standing over <strong>12" tall</strong>, he presents the embodiment of a <strong><em>mighty tribble!</em></strong></p>
 			<div class="parent">
-						<h4>Things Will Likes:</h4>
+						<h2>Things Will Likes:</h2>
 						<ul>
 							<li id="item1">rolling around</li>
 							<li id="item2">multiplying</li>
 							<li id="item3">taking up space</li>
 							<li id="item4">harassing klingons</li>
 						</ul>
-				<img src="./resources/tribble.jpg" alt="Will Tredway" style="width:500px;height:500px;">
 			</div>
+			<img src="./resources/tribble.jpg" alt="Will Tredway" style="width:500px;height:500px;">
 		</div>
 	</body>
 </html>
