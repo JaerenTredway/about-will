2fc572c580cf52ef9964e16d6cd260787ebab5e8
William Isengard <william.isengard@gmail.com>
2019-01-14 16:41:06 -0700

    changed div class to list and updated CSS qualities

--- index.php
+++ index.php
@@ -11,15 +11,15 @@
 		<h1>About Me</h1>
 		<div class="container">
 			<p>Will Tredway is a former carpenter.</p>
-			<p>Standing over <strong>12" tall</strong>, he presents the embodiment of a <strong><em>mighty tribble!</em></strong></p>
-			<div class="parent">
-						<h2>Things Will Likes:</h2>
-						<ul>
-							<li id="item1">rolling around</li>
-							<li id="item2">multiplying</li>
-							<li id="item3">taking up space</li>
-							<li id="item4">harassing klingons</li>
-						</ul>
+			<p>Standing over <strong><em>12" tall</em></strong>, he presents the embodiment of a <strong><em>mighty tribble!</em></strong></p>
+			<div class="list">
+				<h2>Things Will Likes:</h2>
+					<ul>
+						<li id="item1">rolling around</li>
+						<li id="item2">multiplying</li>
+						<li id="item3">taking up space</li>
+						<li id="item4">harassing klingons</li>
+					</ul>
 			</div>
 			<img src="./resources/tribble.jpg" alt="Will Tredway" style="width:500px;height:500px;">
 		</div>
